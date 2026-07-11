8c109995caa7858d355cacf3f5bde0e36df2bcb9
AF <kmhuang2500013079@gmail.com>
2026-07-11 19:22:54 +0800

    Update mainwindow.cpp

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -7,6 +7,7 @@
 #include <QVBoxLayout>
 #include <algorithm>
 #include <random>
+#include <queue>
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
@@ -16,10 +17,10 @@ MainWindow::MainWindow(QWidget *parent)
     ui->setupUi(this);
     startButton->setFixedSize(90, 30);
 
-    generateGrid();
+    generateGridCover();
 
     connect(startButton, &QPushButton::clicked
-            , this, &MainWindow::generateGrid);
+            , this, &MainWindow::generateGridCover);
 
 }
 
@@ -28,12 +29,14 @@ MainWindow::~MainWindow()
     delete ui;
 }
 
-void MainWindow::generateGrid(){
+void MainWindow::generateGridCover() {
     if (central) {
         startButton->setParent(this);
         delete central;
         central = nullptr;
     }
+
+    firstClick = true;
 
     central = new QWidget(this);
     QVBoxLayout *mainVerticalLayout = new QVBoxLayout(central);
@@ -44,63 +47,134 @@ void MainWindow::generateGrid(){
     buttonsLayout->setAlignment(Qt::AlignHCenter);
     mainVerticalLayout->addLayout(buttonsLayout);
 
-    QGridLayout *gridLayout = new QGridLayout();
+    gridLayout = new QGridLayout();
     gridLayout->setSpacing(0);
     gridLayout->setVerticalSpacing(0);
 
-    //set mines
-    std::vector<bool> mines (gridHeight * gridLength, false);
-    for (int i=0; i < mineNumber; ++i) {
-        mines[i] = true;
-    }
-    std::random_device rd;
-    std::mt19937 randomGenerator(rd());
-    std::shuffle(mines.begin(), mines.end(), randomGenerator);
-
     for (int i=0; i < gridHeight; ++i) {
         for (int j=0; j < gridLength; ++j) {
-            Cell *cell = new Cell(i, j, this);
-
-            bool whetherMine = mines[i * gridLength + j];
-            cell->setMine(whetherMine);
-
+            Cell* cell = new Cell(i, j, this);
             connect(cell, &Cell::clicked, this, &MainWindow::onCellClicked);
             gridLayout->addWidget(cell, i, j);
         }
     }
 
-    for (int i=0; i < gridHeight; ++i) {
-        for (int j=0; j < gridLength; ++j) {
-            int countAround = 0;
-            for (int m=std::max(0, i-1); m <= std::min(gridHeight-1, i+1); ++m) {
-                for (int n=std::max(0, j-1); n <= std::min(gridLength-1, j+1); ++n) {
-                    if (i == m && j == n) continue;
-                    if (qobject_cast<Cell*>(gridLayout->itemAtPosition(m, n)->widget())->isMine()) {
-                        countAround++;
+    mainVerticalLayout->addLayout(gridLayout);
+    setCentralWidget(central);
+    central->layout()->setSizeConstraint(QLayout::SetFixedSize);
+    this->adjustSize();
+}
+
+
+void MainWindow::onCellClicked() {
+    Cell *clickedCell = qobject_cast<Cell*>(sender());
+    if (!clickedCell) return;
+
+    //首次点击必为0
+    if (firstClick) {
+        firstClick = false;
+
+        int startRow = clickedCell->getRow();
+        int startCol = clickedCell->getCol();
+
+        std::vector<std::pair<int, int>> safeZone;
+        for (int i = std::max(0, startRow-1);
+             i <= std::min(gridHeight-1, startRow+1);
+             ++i) {
+
+            for (int j=std::max(0, startCol-1);
+                 j <= std::min(gridLength-1, startCol+1);
+                 ++j) {
+                if (i == startRow && j == startCol) continue;
+
+                safeZone.push_back({i, j});
+            }
+        }
+
+        int safeZoneSize = 1 + safeZone.size();
+        std::vector<bool> allMines(gridHeight*gridLength - safeZoneSize
+                                   , false);
+
+        for (int i=0; i < mineNumber; ++i) {
+            allMines[i] = true;
+        }
+
+        std::random_device rd;
+        std::mt19937 randomGenerator(rd());
+        std::shuffle (allMines.begin(), allMines.end(), randomGenerator);
+
+        int index = 0;
+        for (int i=0; i<gridHeight; ++i) {
+            for (int j=0; j<gridLength; ++j) {
+                bool isSafe = false;
+                for (auto safeCell : safeZone) {
+                    if (i == safeCell.first && j == safeCell.second) {
+                        isSafe = true;
+                        break;
                     }
                 }
-                qobject_cast<Cell*>(gridLayout->itemAtPosition(i, j)->widget())->setMinesAround(countAround);
+                Cell* currentCell = qobject_cast<Cell*>
+                    (gridLayout->itemAtPosition(i, j)->widget());
+                if (!isSafe) {
+                    currentCell->setMine(true);
+                    index++;
+                }else{
+                    currentCell->setMine(false);
+                }
             }
         }
     }
 
-    mainVerticalLayout->addLayout(gridLayout);
-
-    mainVerticalLayout->addStretch();
-
-    setCentralWidget(central);
-}
-
-void MainWindow::onCellClicked() {
-    Cell *clickedCell = qobject_cast<Cell*>(sender());
-
     if (clickedCell) {
         if (clickedCell->isOpened()) return ;
         clickedCell->setOpened(true);
+
+        if (clickedCell->countMinesAround() == 0
+            && !clickedCell->isMine()) {
+            expand(clickedCell->getRow(), clickedCell->getCol());
+        }
     }
 
 }
 
+void MainWindow::expand(int startRow, int startCol) {
+    if (!gridLayout) return;
+
+    std::queue<std::pair<int, int>> adjoins;
+    adjoins.push({startRow, startCol});
+
+    while(!adjoins.empty()){
+        auto [curRow, curCol] = adjoins.front();
+        adjoins.pop();
+
+        Cell* currentCell = qobject_cast<Cell*>
+            (gridLayout->itemAtPosition(curRow, curCol)->widget());
+
+        currentCell->setFlagged_expanded(true);
+
+        for (int i = std::max(0, curRow-1);
+             i <= std::min(gridHeight-1, curRow+1);
+             ++i){
+
+            for (int j = std::max(0, curCol-1);
+                 j <= std::min(gridLength-1, curCol+1);
+                 ++j) {
+
+                if (i == curRow && j == curCol) continue;
+
+                Cell* newCell = qobject_cast<Cell*>
+                    (gridLayout->itemAtPosition(i, j)->widget());
+                newCell->setOpened(true);
+                if (newCell->countMinesAround() == 0
+                    && !newCell->isFlagged_expanded()
+                    && !newCell->isMine()) {
+                    adjoins.push({newCell->getRow(), newCell->getCol()});
+                }
+            }
+        }
+    }
+
+}
 
 
 
